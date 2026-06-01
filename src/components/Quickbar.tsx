1b8832d86ed8c69228098899d6b43d624222ac34
Isaac Hollaway <ijhollaw@uncg.edu>
2026-05-31 19:27:10 -0700

    updates to project render
    vite now renders correctly even when not on the production build
    added new svgs
    implemented new svgs

--- src/components/Quickbar.tsx
+++ src/components/Quickbar.tsx
@@ -1,11 +1,16 @@
+import weirdGon from "../assets/WeirdGon.svg"
+import boxes from "../assets/boxes.svg"
+
 function Quickbar() {
     return (
         <div className="flex h-full w-60 border-solid rounded-tr-2xl border-windows-300 bg-white">
-            <div className="indent-10 space-y-3.5 w-full">
-                <div className="pt-1 pb-1 my-2 mx-2 bg-windows-100 rounded">
+            <div className="space-y-3.5 w-full">
+                <div className="flex items-center indent-1 pt-1 pb-1 my-2 mx-2 bg-windows-100 rounded">
+                    <img className="h-4 w-4 ml-1" src={weirdGon} alt="img" />
                     <p>Agents</p>
                 </div>
-                <div className="text-black/60 my-2 mx-2">
+                <div className="flex items-center indent-1 text-black/60 my-2 mx-2">
+                    <img className="h-4 w-4 ml-1" src={boxes} alt="img" />
                     <p>Deployments</p>
                 </div>
                 <div className="text-black/60 my-2 mx-2">

33a8fddb8dac1d9d20a6e91b61400923086779ea
Isaac Hollaway <ijhollaw@uncg.edu>
2026-06-01 12:00:32 -0700

    duplicate styling
    removed duplicate styling

--- src/components/MessageWindow.tsx
+++ src/components/MessageWindow.tsx
@@ -1,6 +1,6 @@
 function MessageWindow() {
     return (
-        <div className="text-left pt-3 pl-3 mx-4 h-26 text-center border border-windows-100 rounded-md bg-windows-100">
+        <div className="text-left pt-3 pl-3 mx-4 h-26 border border-windows-100 rounded-md bg-windows-100">
             <p className=" text-black/60">
                 Message the agent...
             </p>

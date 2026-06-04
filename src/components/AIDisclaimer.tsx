239955e0a6fd03034029a8034a700e41e41bc907
Isaac Hollaway <ijhollaw@uncg.edu>
2026-06-04 15:38:15 -0700

    Update AIDisclaimer.tsx
    typo

--- src/components/AIDisclaimer.tsx
+++ src/components/AIDisclaimer.tsx
@@ -2,7 +2,7 @@ function AIDisclaimer() {
     return (
         <div className="mb-4 pt-1 text-center">
             <p className="text-xs text-black/60">
-                AI-Generated content may be incorrect
+                AI-generated content may be incorrect
             </p>
         </div>
     )
